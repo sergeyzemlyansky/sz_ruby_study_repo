7094c6ddd7a84c2a19c41f4c8725e4dbe6f7b01e
sergey@zemlyansky.me <sergey@zemlyansky.me>
2021-12-12 19:12:45 +0300

    Minor comments

--- Lesson_1/triangle_square.rb
+++ Lesson_1/triangle_square.rb
@@ -1,13 +1,13 @@
-#Calculate triangle square
+#get triangle height
 puts "input triangle height"
 triangle_height=gets.chomp
 triangle_height=triangle_height.to_f
+
+#get triangle foundation
 puts "input triange foundation"
 triangle_foundation=gets.chomp
 triangle_foundation=triangle_foundation.to_f
 
-#calculate triangle square
-
+#calculate and print triangle square
 triangle_square=0.5*triangle_height*triangle_foundation
 puts triangle_square
-
