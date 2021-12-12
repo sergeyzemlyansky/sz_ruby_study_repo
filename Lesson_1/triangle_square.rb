7f5a895663969fdb63a1224df2c7c460ed0c222a
sergey@zemlyansky.me <sergey@zemlyansky.me>
2021-12-12 19:10:40 +0300

    task 2

---
+++ Lesson_1/triangle_square.rb
@@ -0,0 +1,13 @@
+#Calculate triangle square
+puts "input triangle height"
+triangle_height=gets.chomp
+triangle_height=triangle_height.to_f
+puts "input triange foundation"
+triangle_foundation=gets.chomp
+triangle_foundation=triangle_foundation.to_f
+
+#calculate triangle square
+
+triangle_square=0.5*triangle_height*triangle_foundation
+puts triangle_square
+
